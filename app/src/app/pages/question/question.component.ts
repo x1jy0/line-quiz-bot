c2ba9557c9a05f696a1a2ff5fbc8af68538ff260
x1jy0 <x1jy0worker@gmail.com>
2022-08-04 10:54:27 +0900

    単一選択と複数選択のコードとCSSを追加

--- app/src/app/pages/question/question.component.ts
+++ app/src/app/pages/question/question.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
 import liff from '@line/liff';
 import { environment } from 'src/environments/environment';
+
+export interface Task {
+  name: string;
+  completed: boolean;
+  subtasks?: Task[];
+}
 
 @Component({
   selector: 'app-question',
@@ -8,12 +15,41 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./question.component.scss'],
 })
 export class QuestionComponent implements OnInit {
+  single: boolean;
+  multi: boolean;
+  sort: boolean;
+
+  //単一選択のradio-button
+  favoriteSeason: string | undefined;
+  seasons: string[] = [
+    'MSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSI',
+    'DELL',
+    'Apple',
+    'Hewlett-Packard',
+  ];
+
+  //複数選択のmat-checkbox
+  task: Task = {
+    name: 'Indeterminate',
+    completed: false,
+    subtasks: [
+      { name: 'ゴーヤ', completed: false },
+      { name: 'なす', completed: false },
+      { name: 'いちご', completed: false },
+      { name: 'トマト', completed: false },
+    ],
+  };
   constructor() {
     /*
     liff.init({
       liffId: environment.LIFF_ID,
     });
     */
+
+    //問題出題形式の指定
+    this.single = false;
+    this.multi = true;
+    this.sort = false;
   }
 
   ngOnInit(): void {}

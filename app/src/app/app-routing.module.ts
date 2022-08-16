7e15f103cf54e14b8a0024902cdd20622f90d662
x1jy0 <x1jy0worker@gmail.com>
2022-08-17 08:01:01 +0900

    answerコンポーネントのルーティングを追加

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnswerComponent } from './pages/answer/answer/answer.component';
 import { QuestionComponent } from './pages/question/question.component';
 
-const routes: Routes = [{ path: 'question', component: QuestionComponent }];
+const routes: Routes = [
+  { path: 'question', component: QuestionComponent },
+  { path: 'answer', component: AnswerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

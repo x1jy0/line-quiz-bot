424efc9e28ece018632fa8ac5d3f00ac715b76ba
x1jy0 <x1jy0worker@gmail.com>
2022-08-17 08:23:47 +0900

    レイアウトと表示内容の確認用にquestionの中と同じものを仮置き

--- app/src/app/pages/answer/answer/answer.component.ts
+++ app/src/app/pages/answer/answer/answer.component.ts
@@ -1,15 +1,52 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
+
+export interface Task {
+  name: string;
+  completed: boolean;
+  subtasks?: Task[];
+}
 
 @Component({
   selector: 'app-answer',
   templateUrl: './answer.component.html',
-  styleUrls: ['./answer.component.scss']
+  styleUrls: ['./answer.component.scss'],
 })
 export class AnswerComponent implements OnInit {
+  questionType: string;
 
-  constructor() { }
+  //単一選択のradio-button
+  favoriteSeason: string | undefined;
+  seasons: string[] = [
+    'MSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSIMSI',
+    'DELL',
+    'Apple',
+    'Hewlett-Packard',
+  ];
 
-  ngOnInit(): void {
+  //複数選択のmat-checkbox
+  task: Task = {
+    name: 'Indeterminate',
+    completed: false,
+    subtasks: [
+      { name: 'ゴーヤ', completed: false },
+      { name: 'なす', completed: false },
+      { name: 'いちご', completed: false },
+      { name: 'トマト', completed: false },
+    ],
+  };
+  //並び替え問題の内容
+  movies = ['1111111111', '2222222222', '3333333333', '4444444444'];
+
+  constructor() {
+    //問題出題形式の指定
+    this.questionType = 'single';
   }
 
+  //並び替え問題
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+  }
+
+  ngOnInit(): void {}
 }

d9ea80d6506c32099ffe8be7514da24ffcf349d4
x1jy0 <x1jy0worker@gmail.com>
2022-08-17 17:44:52 +0900

    大まかにパーツを配置

--- app/src/app/pages/setting/setting.component.ts
+++ app/src/app/pages/setting/setting.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Item {
+  name: string;
+  completed: boolean;
+  items?: Item[];
+}
+
 @Component({
   selector: 'app-setting',
   templateUrl: './setting.component.html',
-  styleUrls: ['./setting.component.scss']
+  styleUrls: ['./setting.component.scss'],
 })
 export class SettingComponent implements OnInit {
+  //複数選択のmat-checkbox
+  item: Item = {
+    name: 'Indeterminate',
+    completed: false,
+    items: [
+      { name: '1', completed: false },
+      { name: '2', completed: false },
+      { name: '3', completed: false },
+      { name: '4', completed: false },
+      { name: '5', completed: false },
+    ],
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

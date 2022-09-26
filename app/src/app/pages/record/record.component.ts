997ad54dbcc086e7bdda62f67e31ccadcf421e90
x1jy0 <x1jy0worker@gmail.com>
2022-09-26 16:14:27 +0900

    lineLogin処理を実装

--- app/src/app/pages/record/record.component.ts
+++ app/src/app/pages/record/record.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import liff from '@line/liff';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-record',
   templateUrl: './record.component.html',
-  styleUrls: ['./record.component.scss']
+  styleUrls: ['./record.component.scss'],
 })
 export class RecordComponent implements OnInit {
+  log = '';
+  user: any;
+  userIdIndex: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    // lineLogin処理
+    liff
+      .init({
+        liffId: environment.SETTING_LIFF_ID,
+      })
+      .then(() => {
+        // Start to use liff's api
+        // login call, only when external browser or LINE's in-app browser is used
+        if (!liff.isLoggedIn()) {
+          liff.login();
+        } else {
+          liff
+            .getProfile()
+            //getProfileの戻り値
+            .then((profile) => {
+              this.user = profile;
+              console.log('UserData:', this.user);
+            })
+            .catch((err) => {
+              console.log('error', err);
+            });
+        }
+      })
+      .catch((err) => {
+        // Error happens during initialization
+        console.log(err.code, err.message);
+      });
   }
 
+  ngOnInit(): void {}
 }

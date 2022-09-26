dca7b5ee371b3d2b35a9c77424b5767c38d54216
x1jy0 <x1jy0worker@gmail.com>
2022-09-26 16:13:46 +0900

    RecordComponentをapp-routingに追記

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AnswerComponent } from './pages/answer/answer.component';
 import { QuestionComponent } from './pages/question/question.component';
 import { SettingComponent } from './pages/setting/setting.component';
-
+import { RecordComponent } from './pages/record/record.component';
 const routes: Routes = [
   { path: 'question', component: QuestionComponent },
   { path: 'answer', component: AnswerComponent },
   { path: 'setting', component: SettingComponent },
+  { path: 'record', component: RecordComponent },
 ];
 
 @NgModule({

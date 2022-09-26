b77e5c0e9d6ae3063b2ae9247ba5a45d29fb3ec0
x1jy0 <x1jy0worker@gmail.com>
2022-09-26 18:05:59 +0900

    moduleの追加と仮実装

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { QuestionComponent } from './pages/question/question.component';
@@ -32,6 +34,8 @@ import { RecordComponent } from './pages/record/record.component';
     DragDropModule,
     MaterialModule,
     HttpClientModule,
+    MatCardModule,
+    MatProgressBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],

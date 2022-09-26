8330fb6157933e9e483fa94326d57173ea282fb6
x1jy0 <x1jy0worker@gmail.com>
2022-09-26 10:42:54 +0900

    lineLoginとliffアプリの終了処理を実装

--- app/src/app/pages/setting/setting.component.ts
+++ app/src/app/pages/setting/setting.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import liff from '@line/liff';
+import { environment } from 'src/environments/environment';
 export interface Item {
   name: string;
   completed: boolean;
@@ -24,8 +25,43 @@ export class SettingComponent implements OnInit {
       { name: '5', completed: false },
     ],
   };
+  log = '';
+  user: any;
+  userIdIndex: any;
 
-  constructor() {}
+  constructor() {
+    // lineLogin処理
+    liff
+      .init({
+        liffId: environment.SETTING_LIFF_ID,
+      })
+      .then(() => {
+        // Start to use liff's api
+        // login call, only when external browser or LINE's in-app browser is used
+        if (!liff.isLoggedIn()) {
+          liff.login();
+        } else {
+          liff
+            .getProfile()
+            //getProfileの戻り値
+            .then((profile) => {
+              this.user = profile;
+              console.log('UserData:', this.user);
+            })
+            .catch((err) => {
+              console.log('error', err);
+            });
+        }
+      })
+      .catch((err) => {
+        // Error happens during initialization
+        console.log(err.code, err.message);
+      });
+  }
 
   ngOnInit(): void {}
+
+  closeLiff() {
+    liff.closeWindow();
+  }
 }
